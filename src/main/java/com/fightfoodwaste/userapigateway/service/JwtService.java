f56f2a9598336b13b035258a9fdd261bd3630018
svetoslav.hristov <svetoslav.hristov2001@gmail.com>
2024-01-13 18:54:02 +0100

    config changes

--- src/main/java/com/fightfoodwaste/userapigateway/service/JwtService.java
+++ src/main/java/com/fightfoodwaste/userapigateway/service/JwtService.java
@@ -1,15 +1,11 @@
 package com.fightfoodwaste.userapigateway.service;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class JwtService {
